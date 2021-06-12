d6773df1b94937e973df86d3279b06f9692110c1
Camilo_Ponce <caponce4@espe.edu.ec>
2021-06-11 23:47:22 -0500

    update

---
+++ workshops/poncec/unit1/WS06CDCode/FromUmlToCode/src/ec/edu/espe/associations/model/Adress.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.associations.model;
+
+import static javafx.scene.input.KeyCode.F;
+
+/**
+ *
+ * @author Camilo Ponce Program Builder ESPE-DCCO
+ */
+public class Adress 
+{
+private int a;
+private int b;
+
+    /**
+     *
+     * @param e
+     * @return
+     */
+    public Bonds m1(Identification e){
+
+Bonds c=new Bonds();
+    return c;
+}
+
+    /**
+     * @return the a
+     */
+    public int getA() {
+        return a;
+    }
+
+    /**
+     * @param a the a to set
+     */
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    /**
+     * @return the b
+     */
+    public int getB() {
+        return b;
+    }
+
+    /**
+     * @param b the b to set
+     */
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    
+    
+}
