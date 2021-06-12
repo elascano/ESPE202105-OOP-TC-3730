f8660e049b274e6657dc2c64f428d99de604bd70
AlexanderPachacama <84458025+AlexanderPachacama@users.noreply.github.com>
2021-06-12 00:18:20 -0500

    update

---
+++ workshops/pachacamaa/Unit1/WS06DCcode/fromUmlToCode/src/ec/edu/espe/associations/model/C.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.associations.model;
+
+/**
+ *
+ * @author Alexander Pachacama BUILDER ESPE-DCC0
+ */
+public class C {
+    private float f;
+    
+    public void  m()
+
+    /**
+     * @return the f
+     */
+    public float getF() {
+        return f;
+    }
+
+    /**
+     * @param f the f to set
+     */
+    public void setF(float f) {
+        this.f = f;
+    }
+            
+}
