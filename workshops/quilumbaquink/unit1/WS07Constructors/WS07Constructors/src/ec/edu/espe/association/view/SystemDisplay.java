1761198c848e505048938fdf3ac9184709080bc8
knquilumbaquin <knquilumbaquin@espe.edu.ec>
2021-06-14 11:35:31 -0500

    update

---
+++ workshops/quilumbaquink/unit1/WS07Constructors/WS07Constructors/src/ec/edu/espe/association/view/SystemDisplay.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.association.view;
+import ec.edu.espe.association.model.Cat;
+
+/**
+ *
+ * @author Karen Quilumbaquin Program Builder ESPE-DCC0
+ */
+public class SystemDisplay {
+    public static void main(String[] args) {
+            Cat cat = new Cat("Misifuz","gris", 4 , "Siames", 3 ,46);
+            System.out.println(cat);
+    }
+    
+}
