22858c3f414e131686a3595a4fd7bba09c25012a
KerlyMosquera <kemosquera1@espe.edu.ec>
2021-06-14 12:27:14 -0500

    update

---
+++ workshops/mosquerak/unit1/WSO7Contructors/FromUmlToCode/src/ec/edu/espe/constructor/view/Constructor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.constructor.view;
+
+import ec.edu.espe.constructor.model.Laptop;
+
+/**
+ *
+ * @author Kerly Mosquera CODE ESPE-DCCO
+ */
+public class Constructor {
+    
+public static void main(String[] args) {
+      System.out.println("Constructor - Laptop");
+     
+    
+    Laptop laptop = new Laptop("Dell","Latitude",32);
+    System.out.println(laptop);
+     
+    }
+}
