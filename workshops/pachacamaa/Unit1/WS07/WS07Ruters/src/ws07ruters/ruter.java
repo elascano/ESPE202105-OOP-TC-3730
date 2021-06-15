7a74af2458fcf2fba28acf45e8273190ea95a084
AlexanderPachacama <84458025+AlexanderPachacama@users.noreply.github.com>
2021-06-15 09:56:52 -0500

    update

---
+++ workshops/pachacamaa/Unit1/WS07/WS07Ruters/src/ws07ruters/ruter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws07ruters;
+
+/**
+ *
+ * @author Alexander Pachacama BUILDER ESPE-DCC0
+ */
+public class ruter {
+
+    public ruter(String tplink100, String next500, String linsk011) {
+    }
+    
+}
