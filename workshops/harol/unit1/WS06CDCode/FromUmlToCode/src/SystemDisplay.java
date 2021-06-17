26ad7d327a8fb8401ba0068f843fade494d19500
Jorgeguayasamin <aguayasamin18@gmail.com>
2021-06-17 00:07:41 -0500

    update

---
+++ workshops/harol/unit1/WS06CDCode/FromUmlToCode/src/SystemDisplay.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luis Haro LAMESTech ESPE-DCD
+ */
+public class SystemDisplay {
+    public static void main(String[] args) {
+        A a = new A();
+        
+        System.out.printtln("a.b ->" + a.b);
+        
+        System.out.println("From class Diagram to Code,\n" + "Luis Haro");
+}
+}
