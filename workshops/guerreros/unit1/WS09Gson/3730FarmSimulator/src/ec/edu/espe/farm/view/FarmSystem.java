baa7a596798719da435441bbfd5b4837c44210d9
Stefany1-Guerrero <guerrerostefy7@gmail.com>
2021-06-18 16:39:34 -0500

    update

--- workshops/guerreros/unit1/WS09Gson/3730FarmSimulator/src/ec/edu/espe/farm/view/FarmSystem.java
+++ workshops/guerreros/unit1/WS09Gson/3730FarmSimulator/src/ec/edu/espe/farm/view/FarmSystem.java
@@ -5,10 +5,6 @@
  */
 package ec.edu.espe.farm.view;
 
-
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import ec.edu.espe.farm.model.Chicken;
 import java.util.Date;
 
@@ -29,11 +25,7 @@ public class FarmSystem {
         Date bornOn;
         boolean molting;
         int eggCounter;
-       String jsonChicken = ""; 
-       
-       
-        
-       
+             
                  
          id = 0;
          name = "Lucky";
@@ -42,30 +34,14 @@ public class FarmSystem {
          bornOn = new Date();
          molting = true;
          eggCounter = 0;
-       
+        
+               
+        
         chicken = new Chicken(id, name, color, age, bornOn, molting);
         
         System.out.println("chiken->" + chicken);
-        Chicken chicken2 = new Chicken(1,"Camila", "yellow" , new Date(), new Date(), false);
-        System.out.println("chiken->" + chicken2);
         
-         jsonChicken= " { \"id\"; \"1\"    } ";
-         System.out.println("chiken Json -> "+ jsonChicken);
-         
-         
-       GsonBuilder gsonBuilder = new GsonBuilder() ;
-       Gson gson = gsonBuilder.create();
-       jsonChicken = gson.toJson(chicken);
-       
-       System.out.println("jsonChicken ->" +jsonChicken);
-       jsonChicken= gson.toJson(chicken2);
-       System.out.println("jsonChiken ->"+ jsonChicken);
-       
-       Chicken chicken3;
-       chicken3 = gson.fromJson(jsonChicken, Chicken.class);
-       System.out.println(" chicken object name ->"+ chicken3.getName());
-      
-       
+        
     }
     
 
