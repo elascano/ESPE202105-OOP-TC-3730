66650bde0d9ff7d37d03cd2f9b7f474050a06a41
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-06-18 16:37:21 -0500

    update

--- workshops/heredial/unit1/WS09Json/3730ChFarmSimulatorJson/src/ec/ed/espe/farm/view/FarmSystem.java
+++ workshops/heredial/unit1/WS09Json/3730ChFarmSimulatorJson/src/ec/ed/espe/farm/view/FarmSystem.java
@@ -6,6 +6,8 @@
 package ec.ed.espe.farm.view;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import ec.edu.espe.farm.model.Chicken;
 import java.util.Date;
 
@@ -25,26 +27,42 @@ public class FarmSystem {
         Date bornOn;
         boolean molting;
         int eggCounter;
-             
+        
+        String jsonChicken = "";     
                  
          id = 0;
-         name = "Lucky";
-         color = "Withe";
+         name = "Lucy";
+         color = "Black";
          age = new Date();
          bornOn = new Date();
-         molting = true;
-         eggCounter = 0;
-        
-               
+         molting = false;
+         eggCounter = 5;
+      
+                     
         
         chicken = new Chicken(id, name, color, age, bornOn, molting);
         
         System.out.println("chiken->" + chicken);
         
-       Chicken chicken2 = new Chicken(2,"Maruja", "Black" , new Date(), new Date(), false);
+         Chicken chicken2 = new Chicken(2,"Rita", "White" , new Date(), new Date(), true);
         System.out.println("chiken->" + chicken2);
         
+        jsonChicken = "{\"id\";\"1\"}";
+        System.out.println("chicken Json->" + jsonChicken);
         
+         GsonBuilder gsonBuilder=new  GsonBuilder();
+         Gson gson=gsonBuilder.create();
+         
+         jsonChicken = gson.toJson(chicken);
+         System.out.println("jsonChicken ->" + jsonChicken);
+         jsonChicken = gson.toJson(chicken2);
+         System.out.println("jsonChicken ->" + jsonChicken);
+
+         Chicken chicken3;
+         chicken3 = gson.fromJson(jsonChicken, Chicken.class);
+         
+         System.out.println("chicken object name->" + chicken3.getName());
+       
     }
     
 
