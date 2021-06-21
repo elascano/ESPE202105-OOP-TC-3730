2d64e96729e410102f647cc8004e119370e3ab0d
Bryan David <bryanchiliquinga423@gmail.com>
2021-06-21 10:08:58 -0500

    update

--- assignments/chiliquingab/unit1/HW07ChickenGson_CSV/FarmSumulatorGson_CSV/3730ChickenFarmSimulatorGson_CSV/src/ec/edu/espe/farm/view/FarmSystem.java
+++ assignments/chiliquingab/unit1/HW07ChickenGson_CSV/FarmSumulatorGson_CSV/3730ChickenFarmSimulatorGson_CSV/src/ec/edu/espe/farm/view/FarmSystem.java
@@ -5,27 +5,21 @@
  */
 
 package ec.edu.espe.farm.view;
-import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvValidationException;
 import ec.edu.espe.farm.model.Age;
 import static ec.edu.espe.farm.model.Age.calculaAnos;
 import static ec.edu.espe.farm.model.Age.calculaDias;
 import static ec.edu.espe.farm.model.Age.calculaMeses;
 import ec.edu.espe.farm.model.Chicken;
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-import static javafx.scene.input.KeyCode.SEPARATOR;
+import java.io.*;
 
 /**
  *
@@ -49,18 +43,19 @@ public class FarmSystem {
         int option;
         String jsonChicken;
         String jsonAge;
-        FileReader archCSV;
-        CSVReader csvReader;
+        FileReader archCSV = null;
+        CSVReader csvReader = null;
+        String SEPARADOR = null;
         
     //TODO reading from keyboard
     
     ArrayList <Chicken> pollo =  new ArrayList<>();
     ArrayList <Age> edad = new ArrayList<>();
 
-    Chicken chickens[] = new Chicken[1];
-    Age ages[] = new Age[1];
+    Chicken chickens[] = new Chicken[10];
+    Age ages[] = new Age[10];
         
-    for(int i=0;i<1;i++){
+    for(int i=0;i<10;i++){
         System.out.println("\nEnter the chicken data[" + (i+1) + "]:");
         
         System.out.print("Enter the id: ");
@@ -103,7 +98,7 @@ public class FarmSystem {
 //       System.out.println("Chicken -> " + chickens[i]);
 //       System.out.println("El pollo[" + (i+1) + "]" + " tiene la edad de " + ages[i]);
 //  }
-    
+
     GsonBuilder gsonBuilder = new GsonBuilder();
     Gson gson = gsonBuilder.create();
     
@@ -122,7 +117,7 @@ public class FarmSystem {
     
     if(option == 1){
     
-    for(int i=0;i<1;i++){
+    for(int i=0;i<10;i++){
     //Serealizacion
     System.out.println("\njsonChicken[" + (i+1) + "]");
     jsonChicken = gson.toJson(chickens[i]);
@@ -137,7 +132,7 @@ public class FarmSystem {
     
     if(option == 2){
     
-    for(int i=0;i<1;i++){
+    for(int i=0;i<10;i++){
     //Serealizacion
     System.out.println("\njsonChicken[" + (i+1) + "]");
     jsonChicken = gson.toJson(chickens[i]);
@@ -185,33 +180,25 @@ public class FarmSystem {
     
     if(option == 4){
         
-           BufferedReader br = null;
-      
-      try {
-         
-         br =new BufferedReader(new FileReader("C:\\Users\\DELL\\Desktop\\Programacion\\ESPE202105-OOP-TC-3730\\assignments\\chiliquingab\\unit1\\HW07ChickenGson_CSV\\FarmSumulatorGson_CSV\\3730ChickenFarmSimulatorGson_CSV\\Chicken.csv"));
-         String line = br.readLine();
-         while (null!=line) {
-             String SEPARATORRATOR = null;
-            String [] fields = line.split(SEPARATORRATOR);
-            System.out.println(Arrays.toString(fields));
-            
-            fields = removeTrailingQuotes(fields);
-            System.out.println(Arrays.toString(fields));
-            
-            line = br.readLine();   
-         }
-         
-      } catch (IOException e) {
-         
-      } finally {
-         if (null!=br) {
-            br.close();
-         }
-      }
-                
+    System.out.println("\n");
+       
+    try{
+        BufferedReader reader = new BufferedReader(new FileReader("Chicken.csv"));
+        String line;
+            while((line = reader.readLine()) != null){
+                String [] parts = line.split(",");
+                int totalParts = parts.length;
+        
+                for(int i=0;i<totalParts;i++){
+                    System.out.print(parts[i] + ", ");
+                }
+            System.out.print("\n");
+            }
+    }catch(IOException e){
+    e.printStackTrace();
     }
-    
+   
+}
     if(option == 5){
         salir = true;
     }
@@ -220,10 +207,6 @@ public class FarmSystem {
     
         System.out.println("\nEnd of the program");
    
-    }
-
-    private static String[] removeTrailingQuotes(String[] fields) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
   }
