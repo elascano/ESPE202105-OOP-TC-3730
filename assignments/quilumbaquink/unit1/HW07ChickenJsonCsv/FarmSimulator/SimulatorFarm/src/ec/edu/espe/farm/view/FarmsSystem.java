3df4bcfead6fd4ae10d72471baf3f8d1e9885589
knquilumbaquin <knquilumbaquin@espe.edu.ec>
2021-06-20 23:54:37 -0500

    update!

--- assignments/quilumbaquink/unit1/HW07ChickenJsonCsv/FarmSimulator/SimulatorFarm/src/ec/edu/espe/farm/view/FarmsSystem.java
+++ assignments/quilumbaquink/unit1/HW07ChickenJsonCsv/FarmSimulator/SimulatorFarm/src/ec/edu/espe/farm/view/FarmsSystem.java
@@ -5,10 +5,9 @@
  */
 package ec.edu.espe.farm.view;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import ec.edu.espe.farm.model.Born;
 import ec.edu.espe.farm.model.Chicken1;
+
 import java.util.Date;
 import java.util.Scanner;
 
@@ -18,15 +17,7 @@ import java.util.Scanner;
  */
 public class FarmsSystem {
     public static void main(String[] args) {
-        
         Scanner read=new Scanner(System.in);
-        int option;
-        System.out.print("\n 1.Save json ");
-        System.out.print("\n 2.Read json  ");
-        System.out.print("\n 3.Save csv ");
-        System.out.print("\n 4.Read csv ");
-        System.out.print("\n Enter a option: ");
-        option=read.nextInt();
         Chicken1 chicken;
         Born born;
         //reading from keyboard
@@ -39,8 +30,8 @@ public class FarmsSystem {
         int eggCounter;
         
         
-        Chicken1 [] chickens=new Chicken1[1];
-        Born [] bornOn1=new Born[1];
+        Chicken1 [] chickens=new Chicken1[2];
+        Born [] bornOn1=new Born[2];
         
         for(int i=0;i<chickens.length;i++){
             System.out.print("Chicken[%d] "+ i);
@@ -64,55 +55,23 @@ public class FarmsSystem {
             int año=2021-year;
             age = new Date();
             bornOn = new Date();
-            chicken = new Chicken1(id,name,color,age,bornOn,molting,eggCounter);
+            chicken = new Chicken1(id,name,color,age,bornOn,molting);
             chickens[i]= chicken;
             born= new Born(day,month,year,año);
             bornOn1[i]= born;
+            
+            
+            //System.out.println("chicken ->" + chicken);
         
         }
-        for (int j=0; j<1; j++){
+        for (int j=1; j<=2; j++){
             
             System.out.println("chicken"+ j + "=" + chickens[j]);
             System.out.println("BorOn "+ j + "=" + bornOn1[j]);
             
             
         }
-        
-        switch(option){
-            case 1:
-                GsonBuilder gsonBuilder= new GsonBuilder();
-                Gson gson=gsonBuilder.create();
-        
-                String jsonChicken="{\"id\":\"1\"}";
-                System.out.println("chicken Json ->"+jsonChicken);
-        
-                jsonChicken=gson.toJson(chickens);
-        
-                //Serialization
-                System.out.println("jsonChicken ->"+jsonChicken);
-                //jsonChicken=gson.toJson(chickens);
-                //System.out.println("jsonChicken ->"+jsonChicken);
-       
-        
-                //Deserialization
-                
-                Chicken1 chicken3[]=new Chicken1[1];
-                chicken3[0]=gson.fromJson(jsonChicken,Chicken1.class);
-                System.out.println("chicken object name ->"+chicken3[0].getId());
-            break;
-            case 2:
-            break;
-            case 3:
-            break;
-            case 4:
-            break;
-            
-        }
-        
-        
     }
-    
-         
             
     
 }
