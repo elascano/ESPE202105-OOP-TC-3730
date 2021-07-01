397772a19437746f7184d876a6a226df93349021
Kevin Barrera <kabarrera3@espe.edu.ec>
2021-06-30 19:46:49 -0500

    Workshop

--- workshops/barrerak/Unit 2/WS13_OBJECT/Smartphonesimulation/src/SmartPhone/view/Smartphonesimulation.java
+++ workshops/barrerak/Unit 2/WS13_OBJECT/Smartphonesimulation/src/SmartPhone/view/Smartphonesimulation.java
@@ -3,66 +3,68 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.espe.exam.view;
+package SmartPhone.view;
 
-import ec.edu.espe.exam.model.Book;
+import SmartPhone.model.Smartphone;
 import java.io.FileWriter;
 import java.io.IOException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.util.Scanner;
-
-
-
 
 
 /**
  *
  * @author Kevin Barrera Skynet.hub tech ESPE-DCCO
  */
-public class KevinBarreraExam1 {
+public class Smartphonesimulation {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-              Book book;
+        
+        Smartphone smartphone ;
         //TODO reading from keyboard
-        int id;
-        String author;
-        String editorial;
-        int yearPublication;
+        String brand;
+        String model;
+        int ram;
+        int rom;
+        String color;
         String title;
-        String jsonBook = "";
+     
+        String jsonSmartphone = "";
         Scanner read = new Scanner(System.in);
         int optionMenu;
-        FileWriter file = new FileWriter("./files/Books.json");
-        System.out.println("Welcome to books system Kevin");
+        FileWriter file = new FileWriter("./files/Smartphones.json");
+        System.out.println("Welcome to Smatphone system Kevin");
         do{
-        System.out.println("1.- Save Book");
-        System.out.println("2.- List Book");
-        System.out.println("3.- Exit");
+        System.out.println("1.- Smartphones");
+        System.out.println("2.- Exit");
+        
         System.out.println("Choose the option: ");
         optionMenu = read.nextInt();
         switch (optionMenu) {
             case 1:
-                System.out.println("Introduce id of Book: ");
-                id = read.nextInt();
-                System.out.println("Introduce title of book: ");
-                title = read.next();
-                System.out.println("Introduce author of book: ");
-                author = read.next();
-                System.out.println("Introduce editorial of book: ");
-                editorial = read.next();
-                System.out.println("Introduce year of publication of book: ");
-                yearPublication = read.nextInt();
-                book = new Book(id,title,author,editorial,yearPublication);
+                System.out.println("Introduce brand of the Smartphone: ");
+                brand = read.next();
+                System.out.println("Introduce model of the Smartphone: ");
+                model = read.next();
+                System.out.println("Introduce the Ram that you want: ");
+                ram = read.nextInt();
+                System.out.println("Introduce Rom that you want: ");
+                rom = read.nextInt();
+                System.out.println("Introduce the color of the Smartphone: ");
+                color = read.next();
+                
+                smartphone = new Smartphone(brand, model, ram, rom, color);
                 GsonBuilder gsonBuilder = new GsonBuilder();
                 Gson gson = gsonBuilder.create();
-                jsonBook = gson.toJson(book);
+                jsonSmartphone = gson.toJson(smartphone);
                 try {
-                    file.append(jsonBook);
+                    file.append(jsonSmartphone);
                     file.flush();
                     file.close();
 
@@ -71,18 +73,13 @@ public class KevinBarreraExam1 {
                 }
                 break;
             case 2:
-                
-                break;
-            case 3:
                 System.out.println("Exit of system");
                 break;
             default:
                 System.out.println("option not valid...!!!");
         }
-        }while(optionMenu!=3);
+        }while(optionMenu!=2);
 
     }
- 
-}
     
-
+}
