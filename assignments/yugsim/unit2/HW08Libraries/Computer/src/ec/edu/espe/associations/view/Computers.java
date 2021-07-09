b3a90a1dfecce50353a4c15231391c5ada07b50d
MPamelaYugsi <mpyugsi@espe.edu.ec>
2021-07-09 18:20:56 -0500

    Fix HW08

--- assignments/yugsim/unit2/HW08Libraries/Computer/src/ec/edu/espe/associations/view/Computers.java
+++ assignments/yugsim/unit2/HW08Libraries/Computer/src/ec/edu/espe/associations/view/Computers.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.associations.view;
+package ec.edu.espe.associations.view;
 
 import ec.edu.espe.computer.*;
 import java.io.IOException;
