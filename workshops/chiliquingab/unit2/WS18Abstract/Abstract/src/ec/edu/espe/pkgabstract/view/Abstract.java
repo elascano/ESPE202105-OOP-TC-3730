25297da18879a2b2003e9f87a8de2089b03aa9e8
Bryan David <bryanchiliquinga423@gmail.com>
2021-07-12 12:20:45 -0500

    WS18 Abstract

--- workshops/chiliquingab/unit2/WS18Abstract/Abstract/src/ec/edu/espe/pkgabstract/view/Abstract.java
+++ workshops/chiliquingab/unit2/WS18Abstract/Abstract/src/ec/edu/espe/pkgabstract/view/Abstract.java
@@ -5,11 +5,6 @@
  */
 package ec.edu.espe.pkgabstract.view;
 
-import ec.edu.espe.pkgabstract.model.B;
-import ec.edu.espe.pkgabstract.model.D;
-import ec.edu.espe.pkgabstract.model.E;
-import java.util.ArrayList;
-
 /**
  *
  * @author Bryan Chiliquinga Beta_Software ESPE-DCCO
@@ -20,22 +15,7 @@ public class Abstract {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Abstract Example in class");
-        
-        B b;
-        
-        b = new D(8, 32);
-        System.out.println("b in int is -> " + b);
-        
-        System.out.println("b int is a class of -> " + b);
-        
-        b = new E(2.2F, 0.8F);
-        System.out.println("b in float is -> " + b.getClass());
-        
-        ArrayList<B> bs = new ArrayList<>();
-        bs.add(b);
-        
-        System.out.println("b float is a class of -> " + b.getClass());
+        System.out.println("Abstract Example Bryan Chiliquinga NRC: 3027");
+
     }
-    
 }
