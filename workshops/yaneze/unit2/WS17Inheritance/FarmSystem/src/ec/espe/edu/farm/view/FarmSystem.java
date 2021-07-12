cf51c3aeb387343a39288770ad54127cd4915362
erickyanez1 <eyanez2002@hotmail.com>
2021-07-12 11:46:35 -0500

    ws18abstract

--- workshops/yaneze/unit2/WS17Inheritance/FarmSystem/src/ec/espe/edu/farm/view/FarmSystem.java
+++ workshops/yaneze/unit2/WS17Inheritance/FarmSystem/src/ec/espe/edu/farm/view/FarmSystem.java
@@ -7,6 +7,7 @@ package ec.espe.edu.farm.view;
 
 import ec.espe.edu.farm.model.Chicken;
 import ec.espe.edu.farm.model.FarmAnimal;
+import ec.espe.edu.farm.model.Pig;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -22,7 +23,7 @@ public class FarmSystem {
     public static void main(String[] args) {
         FarmAnimal farmAnimal;
         Date date = new Date();
-        farmAnimal = new FarmAnimal(0, "german shepherd", date);
+        farmAnimal = new Chicken(0, "criolla", date, false);
         System.out.println("FarmAnimal: " + farmAnimal);
         
         date = new Date(2021, 1, 20);
@@ -47,6 +48,17 @@ public class FarmSystem {
         System.out.println("My farm has -> " + farmAnimals.size() + " animals");
         System.out.println("My FarmAnimals are -> " + farmAnimals);
     
+        farmAnimal = new Pig(20.5F, 3, "miniature", date);
+        farmAnimals.add(farmAnimal);
+        
+        
+        chicken.feed(1);
+        farmAnimal.feed(5);
+        
+        chicken.vaccinate(3);
+        farmAnimal.vaccinate(1);
+        
+        System.out.println("My Farm Animals are " + farmAnimals);
     }
     
 }
