36268b3d2ec4775d84f23e4d38aac06301a89909
Gabriela-Sunta <jgsunta@espe.edu.ec>
2021-07-12 20:21:43 -0500

    Abstract Classes

---
+++ workshops/suntaj/unit2/WS18Abstract/AbstractClasses/src/ec/edu/espe/abastract/model/B.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.abastract.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gabriela Sunta Future'sProgrammersTech ESPE- DCCO
+ */
+public abstract class B {
+    private float f;
+    private double d;
+    
+     private ArrayList<G> gs = new ArrayList<>();
+     
+     
+     public int operation1(int n){
+         
+        return n;
+     }
+     
+     public float operation2(float f){
+         
+         return f;
+     }        
+
+    /**
+     * @return the f
+     */
+    public float getF() {
+        return f;
+    }
+
+    /**
+     * @param f the f to set
+     */
+    public void setF(float f) {
+        this.f = f;
+    }
+
+    /**
+     * @return the d
+     */
+    public double getD() {
+        return d;
+    }
+
+    /**
+     * @param d the d to set
+     */
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    /**
+     * @return the gs
+     */
+    public ArrayList<G> getGs() {
+        return gs;
+    }
+
+    /**
+     * @param gs the gs to set
+     */
+    public void setGs(ArrayList<G> gs) {
+        this.gs = gs;
+    }
+     
+             
+             
+             
+             
+             }
