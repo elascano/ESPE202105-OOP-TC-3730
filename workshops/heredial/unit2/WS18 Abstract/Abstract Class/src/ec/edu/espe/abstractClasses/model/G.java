767f8870e0469cb473da35187529a80a48c3089a
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-07-12 21:07:46 -0500

    Abstrac Classes

---
+++ workshops/heredial/unit2/WS18 Abstract/Abstract Class/src/ec/edu/espe/abstractClasses/model/G.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.abstractClasses.model;
+
+/**
+ *
+ * @author Luis Heredia Accent on the Future ESPE-DCC0
+ */
+public class G {
+    
+    private int z;
+
+    public G(int z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "G{" + "z=" + getZ() + '}';
+    }
+
+    /**
+     * @return the z
+     */
+    public int getZ() {
+        return z;
+    }
+
+    /**
+     * @param z the z to set
+     */
+    public void setZ(int z) {
+        this.z = z;
+    }
+    
+    
+    
+}
