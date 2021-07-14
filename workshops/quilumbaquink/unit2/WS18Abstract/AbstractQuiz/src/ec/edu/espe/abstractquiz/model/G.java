238d27405730097695c6e3f72b3920a0d82727e1
knquilumbaquin <knquilumbaquin@espe.edu.ec>
2021-07-14 11:44:15 -0500

    WS18

---
+++ workshops/quilumbaquink/unit2/WS18Abstract/AbstractQuiz/src/ec/edu/espe/abstractquiz/model/G.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.abstractquiz.model;
+
+/**
+ *
+ * @author Karen Quilumbaquin Program Builder ESPE-DCC0
+ */
+public class G {
+    private int z;
+
+    public G(int z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "G{" + "z=" + z + '}';
+    }
+    
+    
+
+    /**
+     * @return the z
+     */
+    public int getZ() {
+        return z;
+    }
+
+    /**
+     * @param z the z to set
+     */
+    public void setZ(int z) {
+        this.z = z;
+    }
+    
+    
+}
