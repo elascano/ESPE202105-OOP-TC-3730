b2247f7245c932ca31b3cdf92afd1344e1243ead
Victor <victorecu_@hotmail.com>
2021-07-21 21:53:26 -0500

    WS19-OperationTest

---
+++ workshops/cobav/unit2/WS18-Abstract/WS18Abstract/src/ec/edu/espe/Abstract/model/B.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.Abstract.model;
+
+/**
+ *
+ * @author Victor Coba Beta Software ESPE-DCCD
+ */
+public class B {
+    private float f;
+    private double c;
+}
