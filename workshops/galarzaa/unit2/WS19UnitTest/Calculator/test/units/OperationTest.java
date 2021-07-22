8cfa3404939d32738453a32dbbc23c1c2874bc88
AndresGalarza23 <andryu_0306@hotmail.com>
2021-07-21 23:42:45 -0500

    WS19UnitTest

---
+++ workshops/galarzaa/unit2/WS19UnitTest/Calculator/test/units/OperationTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package units;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Andres Galarza AccentOnTheFuture ESPE-DCCO
+ */
+public class OperationTest {
+    
+    public OperationTest() {
+    }
+
+    /**
+     * Test of add method, of class Operation.
+     */
+    @Test
+    public void testAddZeroe() {
+        System.out.println("add");
+        float addend1 = 0.0F;
+        float addend2 = 0.0F;
+        float expResult = 0.0F;
+        float result = Operation.add(addend1, addend2);
+        assertEquals(expResult, result, 0.0);
+       
+    }
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        float addend1 = 5.0F;
+        float addend2 = 3.0F;
+        float expResult = 8.0F;
+        float result = Operation.add(addend1, addend2);
+        assertEquals(expResult, result, 0.0);
+       
+    }
+    @Test
+    public void testAddPosutivos() {
+        System.out.println("add");
+        float addend1 = 1.20F;
+        float addend2 = 3.40F;
+        float expResult = 4.60F;
+        float result = Operation.add(addend1, addend2);
+        assertEquals(expResult, result, 0.0);
+       
+    }
+
+    /**
+     * Test of subtract method, of class Operation.
+     */
+    @Test
+    public void testSubtract() {
+        System.out.println("subtract");
+        float minuend = 0.0F;
+        float subtrahend = 0.0F;
+        float expResult = 0.0F;
+        float result = Operation.subtract(minuend, subtrahend);
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    
+}
