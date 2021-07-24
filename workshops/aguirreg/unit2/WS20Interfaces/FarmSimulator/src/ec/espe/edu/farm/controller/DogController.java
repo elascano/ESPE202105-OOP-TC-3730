30e1f336cca7c89344dce79c77adee55ea518d17
gabrielaguirre33 <almachesebastian@gmail.com>
2021-07-23 19:25:35 -0500

    WS20Interfaces

---
+++ workshops/aguirreg/unit2/WS20Interfaces/FarmSimulator/src/ec/espe/edu/farm/controller/DogController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.farm.controller;
+
+import ec.espe.edu.farm.model.Location;
+import ec.espe.edu.farm.model.Sound;
+
+/**
+ *
+ * @author Gabriel Aguirre SkyNet.hub ESPE-DCCO
+ */
+public class DogController implements IAnimal {
+
+    @Override
+    public Sound makeSound(int time) {
+        Sound sound = new Sound();
+        System.out.println(" Barking for " + time + " seconds");
+        return sound;    
+    }
+
+    @Override
+    public void eat(float amount) {
+        System.out.println(" Dog is eating " + amount + " crockets");
+    }
+
+    @Override
+    public Location move(int dx, int dy) {
+        Location location = new Location(dx, dy);
+        System.out.println(" The dog is walking "+ dx + " , " + dy );
+        return location;
+    }
+    
+}
