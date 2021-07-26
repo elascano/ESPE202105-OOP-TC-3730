e502a7dfa1bd038e5b6e18f1b93e80ff2e4febed
bryan-steven-maisincho <bsmaisincho@espe.edu.ec>
2021-07-25 20:26:20 -0500

    HW12-Graphical mode interface

--- workshops/maisinchob/unit2/WS20-Interfaces/FarmSimulator/src/ec/edu/espe/farm/view/ChickenFarmer.java
+++ workshops/maisinchob/unit2/WS20-Interfaces/FarmSimulator/src/ec/edu/espe/farm/view/ChickenFarmer.java
@@ -9,12 +9,12 @@ package ec.edu.espe.farm.view;
  *
  * @author Bryan Maisincho Codes ESPE-DCCO
  */
-public class FarmAnimal extends javax.swing.JFrame {
+public class ChickenFarmer extends javax.swing.JFrame {
 
     /**
-     * Creates new form FarmAnimal
+     * Creates new form ChickenFarmer
      */
-    public FarmAnimal() {
+    public ChickenFarmer() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class FarmAnimal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FarmAnimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChickenFarmer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FarmAnimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChickenFarmer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FarmAnimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChickenFarmer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FarmAnimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ChickenFarmer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FarmAnimal().setVisible(true);
+                new ChickenFarmer().setVisible(true);
             }
         });
     }
