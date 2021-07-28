315a459512526cbece25932a786406596a8314e7
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-07-28 16:16:49 -0500

    WS21GUI

---
+++ workshops/heredial/unit2/WS21GUI/TelecomunicationsInventory/src/ec/edu/espe/contacts/model/Product.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.contacts.model;
+
+/**
+ *
+ * @author Luis Heredia Accent on the Future ESPE-DCC0
+ */
+public class Product {
+    
+    private String pname;
+    private int id;
+    private float price;
+    private String brand;
+
+    public Product(String pname, int id, float price, String brand) {
+        this.pname = pname;
+        this.id = id;
+        this.price = price;
+        this.brand = brand;
+    }
+    
+   
+
+    /**
+     * @return the pname
+     */
+    public String getPname() {
+        return pname;
+    }
+
+    /**
+     * @param pname the pname to set
+     */
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+   
+    
+    
+}
+
+    
+    
+
+    
+
