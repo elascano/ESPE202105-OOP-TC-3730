f9be21a5ea4268a610a2a8bb9da993dac6a51c0b
gabrielaguirre33 <almachesebastian@gmail.com>
2021-07-28 19:28:46 -0500

    WS12GUI

---
+++ workshops/aguirreg/unit2/WS21GUI/WS21GUI/src/ec/espe/edu/contacts/model/Gender.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.contacts.model;
+
+/**
+ *
+ * @author Gabriel Aguirre SkyNet.hub ESPE-DCCO
+ */
+public class Gender {
+    private int id;
+    private String description;
+    private boolean gender;
+
+    public Gender(int id, String description, boolean gender) {
+        this.id = id;
+        this.description = description;
+        this.gender = gender;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the gender
+     */
+    public boolean isGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+    
+}
