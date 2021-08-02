3af479f84f4fd686f332599f2bc61ac0bbaafbf6
LilianIza <Leiza1@espe.edu.ec>
2021-08-02 11:57:59 -0500

    QuizUnit2

---
+++ exams/izal/unit2/QuizUnit2/QuizUnit2/src/ec/edu/espe/quizunit2/model/D.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.quizunit2.model;
+
+/**
+ *
+ * @author LILIAN IZA TOURIST GUEST OPP-ESPE
+ */
+public class D {
+    
+}
