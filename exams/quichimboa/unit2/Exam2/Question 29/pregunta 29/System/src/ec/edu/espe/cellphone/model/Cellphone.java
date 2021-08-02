95dd106444cccd04b390ee81955619e96f0723b6
unknown <aequichimbo@espe.edu.ec>
2021-08-02 12:05:23 -0500

    update

---
+++ exams/quichimboa/unit2/Exam2/Question 29/pregunta 29/System/src/ec/edu/espe/cellphone/model/Cellphone.java
@@ -0,0 +1,224 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.cellphone.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Andrea Quichimbo InnovaCode ESPE-DCCO
+ */
+public class Cellphone {
+    
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String addres;
+    private String description;
+    private String gender;
+    private String brand;
+    private String type;
+    private String model;
+    private ArrayList<Additions> additions = new ArrayList<>();
+    private LocalDateTime birthDate;
+    private int cellphonesarecarried;
+
+    public Cellphone(String aac, String escobar, String string, String iaescobarespeeduec, String sangolqui, String client, String male, String samsung, String s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Cellphone() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the addres
+     */
+    public String getAddres() {
+        return addres;
+    }
+
+    /**
+     * @param addres the addres to set
+     */
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the additions
+     */
+    public ArrayList<Additions> getAdditions() {
+        return additions;
+    }
+
+    /**
+     * @param additions the additions to set
+     */
+    public void setAdditions(ArrayList<Additions> additions) {
+        this.additions = additions;
+    }
+
+    /**
+     * @return the birthDate
+     */
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    /**
+     * @param birthDate the birthDate to set
+     */
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    /**
+     * @return the cellphonesarecarried
+     */
+    public int getCellphonesarecarried() {
+        return cellphonesarecarried;
+    }
+
+    /**
+     * @param cellphonesarecarried the cellphonesarecarried to set
+     */
+    public void setCellphonesarecarried(int cellphonesarecarried) {
+        this.cellphonesarecarried = cellphonesarecarried;
+    }
+    
+    
+    
+}
