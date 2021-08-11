2cd680d1e5302dfb463c114112fea0331e734d96
Alina-13 <apvillavicencio1@espe.edu.ec>
2021-08-11 17:35:24 -0500

    Inspector

---
+++ exams/tupizas/unit2/Exam2/src/ec/edu/espe/exam/model/J.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.exam.model;
+
+/**
+ *
+ * @author Solange Tupiza search for electrical and electronic materials
+ * OOP-ESPE
+ */
+public class J {  //0.8
+
+    public J() {
+    }
+    
+    
+}
