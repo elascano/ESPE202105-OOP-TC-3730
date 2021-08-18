40d071a78a86660755b9957d1ed9b874efe9dd82
Vanessa Cayambe <vkcayambe@espe.edu.ec>
2021-08-17 22:31:33 -0500

    HW15-JTable

--- workshops/cayambev/Unit 3/WS24RegistrationSystem/Documents/src/ec/edu/espe/document/view/FrmDocument.java
+++ workshops/cayambev/Unit 3/WS24RegistrationSystem/Documents/src/ec/edu/espe/document/view/FrmDocument.java
@@ -9,12 +9,12 @@ package ec.edu.espe.document.view;
  *
  * @author Vanessa Cayambe Team of machine ESPE-DCCO
  */
-public class FrmArticle extends javax.swing.JFrame {
+public class FrmDocument extends javax.swing.JFrame {
 
     /**
      * Creates new form FrmArticle
      */
-    public FrmArticle() {
+    public FrmDocument() {
         initComponents();
     }
 
@@ -35,7 +35,7 @@ public class FrmArticle extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Article");
+        jLabel1.setText("Document");
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
@@ -100,20 +100,21 @@ public class FrmArticle extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmArticle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmDocument.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmArticle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmDocument.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmArticle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmDocument.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmArticle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmDocument.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmArticle().setVisible(true);
+                new FrmDocument().setVisible(true);
             }
         });
     }
