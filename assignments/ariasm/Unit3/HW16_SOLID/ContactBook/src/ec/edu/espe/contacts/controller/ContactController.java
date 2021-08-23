77fd94952c34f9163c74659d19a9770e478bfa01
MarcoArias191 <maarias20@espe.edu.ec>
2021-08-22 21:12:54 -0500

    HW

---
+++ assignments/ariasm/Unit3/HW16_SOLID/ContactBook/src/ec/edu/espe/contacts/controller/ContactController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.contacts.controller;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import ec.edu.espe.contacts.model.Book;
+import ec.edu.espe.contacts.model.Contact;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Marco Arias SkyNet.hub tech ESPE-DCCO
+ */
+public class ContactController {
+
+    DB DataBase;
+    DBCollection collection;
+    BasicDBObject document = new BasicDBObject();
+
+    public ContactController() {
+
+        try {
+            Mongo mongo = new Mongo("localhost", 27017);
+            DataBase = mongo.getDB("ContactsBook");
+            collection = DataBase.getCollection("Contacts");
+            System.out.println("Welcome - successful connection");
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ContactController.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+
+    public boolean addContactToBook(String name, Integer numberOfFriends, float salary, String email, String book) {
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+        contacts.add(new Contact(name, numberOfFriends, salary, email, book));
+
+        for (Contact pueC : contacts) {
+            collection.insert(pueC.dbContact());
+        }
+        
+        int input = JOptionPane.showConfirmDialog(null, "Has been successfully registered", "OK", JOptionPane.DEFAULT_OPTION);
+        System.out.println(input);
+
+        return true;
+
+    }
+}
