160544f3c61606752e2f12ec2c28234d43b54a16
KerlyMosquera <kemosquera1@espe.edu.ec>
2021-08-27 23:26:08 -0500

    WS27

--- assignments/mosquerak/unit3/HW16-SolidPrincipals/Contacts Book/src/ContactController.java
+++ assignments/mosquerak/unit3/HW16-SolidPrincipals/Contacts Book/src/ContactController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.espe.contacts.controller;
+
 
 import ec.edu.espe.contacts.model.Book;
 import ec.edu.espe.contacts.model.Contact;
