8b19a01bb5a6599c8b4bc6aa9e202ba759f15124
knquilumbaquin <84364011+knquilumbaquin@users.noreply.github.com>
2021-08-27 21:14:37 -0500

    WS27

---
+++ workshops/quilumbaquink/unit3/WS27Hospital/UmbrellaCorp/src/ec/edu/espe/hospital/model/Patient.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.hospital.model;
+
+/**
+ *
+ * @author AnitaQ
+ */
+public class Patient {
+    private String name;
+    private String lastName;
+    private int age;
+    private int id;
+    private String gender;
+
+    public Patient(String name, String lastName, int age, int id, String gender) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.id = id;
+        this.gender = gender;
+    }
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+}
