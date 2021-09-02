5f30cbcf244c726ea1355217ed8fed1e7c85a469
cristophersarm1 <cristopher5619@gmail.com>
2021-09-01 20:09:28 -0500

    WS28

---
+++ workshops/sarmientoc/unit3/WS28SortingAlgorithms/SortingAlgorithms/src/ec/edu/espe/controller/QuickSort.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.controller;
+
+/**
+ *
+ * @author Cristopher Sarmiento Futures Programmers ESPE-DCC0
+ */
+public class QuickSort implements SortingStrategy {
+    
+    @Override
+    public int[] sort(int[] data) {
+        System.out.println("ordering" + data +", using Quick sort");
+        int[] sortedData = data;
+        return sortedData;
+    }
+    
+}
