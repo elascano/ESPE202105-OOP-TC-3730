293344515aa72257a772568c925160bd3dbe41f2
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-09-06 11:51:16 -0500

    EXAM UNIT 3

--- exams/heredial/unit3/EXAMN QUESTION/SortingAlgorithms/src/ec/edu/espe/controller/SortingContext.java
+++ exams/heredial/unit3/EXAMN QUESTION/SortingAlgorithms/src/ec/edu/espe/controller/SortingContext.java
@@ -13,29 +13,29 @@ public class SortingContext {
 
     private SortingStrategy sortingStrategy;
     
-    public int[] sort(int data []){
+  public int[] sort(int data[]) {
         int size = data.length;
         sortingStrategy = setSortStrategy(size);
         return sortingStrategy.sort(data);
     }
 
     public SortingStrategy setSortStrategy(int n) {
-        
-        if(n>0 && n <5){
+
+        if (n > 0 && n < 3) {
             sortingStrategy = new BubbleSort();
- 
-    }
-        if(n>5 && n <10){
+        }
+
+        if (n >= 4 && n <= 7) {
             sortingStrategy = new InsertionSort();
         }
-        if(n>= 10){
-            sortingStrategy = new QuickSort();
- 
 
-    }
+        if (n > 7) {
+            sortingStrategy = new QuickSort();
+        }
 
         return sortingStrategy;
-}
+
+    }
 
    
 }
